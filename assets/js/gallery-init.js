bb3fdfa629540dc0d7d918eb94599734a28a59d0
Mihael Milanović <mihaelmilanovic555@gmail.com>
2026-03-04 17:10:47 +0100

    Added thumbnails to all images.
    Less loading for large images, and only loads on click.

--- assets/js/gallery-init.js
+++ assets/js/gallery-init.js
@@ -10,24 +10,7 @@ const initGallery = () => {
         maxZoomLevel: 1.5,
         doubleTapAction: 'zoom',
         pinchToClose: false,
-    });
-
-    lightbox.addFilter('itemData', (itemData) => {
-        const img = new Image();
-        img.src = itemData.src;
-        if (img.complete) {
-            itemData.width = img.width;
-            itemData.height = img.height;
-        } else {
-            itemData.width = 2000;
-            itemData.height = 2000;
-            img.onload = () => {
-                itemData.width = img.width;
-                itemData.height = img.height;
-                if (lightbox.pswp) lightbox.pswp.refreshSlideContent(itemData.index);
-            };
-        }
-        return itemData;
+        preload: [1, 1] // preload only previous/next images
     });
 
     lightbox.init();
@@ -38,29 +21,3 @@ if (document.readyState === 'loading') {
 } else {
     initGallery();
 }
-
-// GIF Handling
-const gifs = document.querySelectorAll(".gallery-item img, .featured img");
-gifs.forEach(img => {
-    if (!img.src.toLowerCase().endsWith(".gif")) return;
-    const gifSrc = img.src;
-    const staticImg = new Image();
-    staticImg.crossOrigin = "Anonymous";
-    staticImg.src = gifSrc;
-    staticImg.onload = () => {
-        const canvas = document.createElement("canvas");
-        canvas.width = staticImg.width;
-        canvas.height = staticImg.height;
-        const ctx = canvas.getContext("2d");
-        ctx.drawImage(staticImg, 0, 0);
-        img.dataset.static = canvas.toDataURL("image/png");
-        img.src = img.dataset.static;
-        img.dataset.gif = gifSrc;
-    };
-    img.addEventListener("mouseenter", () => {
-        if (img.dataset.gif) img.src = img.dataset.gif;
-    });
-    img.addEventListener("mouseleave", () => {
-        if (img.dataset.static) img.src = img.dataset.static;
-    });
-});
